9fee08e4570f08d198ae148c487cd701442ef77e
ygalves <yoniliman.galves@uao.edu.co>
2024-03-29 10:46:55 -0500

    Fix networks tier between dockers

--- front_end/html/index.php
+++ front_end/html/index.php
@@ -8,22 +8,32 @@
 
 echo "<pre>IA TASKFORCE!\n";
 echo "<pre>WEBSCRAPPING APPLICATION\n";
-echo "<pre>1\n";
+echo "<pre> docker-mariadb \n";
 
-$host       = "127.0.0.1:9004"; // service name of the database container
-$user       = "webusr"; // MARIADB_USER
-$password   = "webusr"; // MARIADB_PASSWORD
+$host       = "docker-mariadb"; // service name of the database container
+$user       = "root"; // MARIADB_USER
+$password   = "root"; // MARIADB_PASSWORD
 $db         = "wsdb"; // MARIADB_DATABASE
 $table      = "user_name"; // table name from our data.sql dump
+$sql        = "SELECT * FROM $table";
 $g_link = false;
-    
+
+
 function GetMyConnection()
 {
+    echo "global.\n";
     global $g_link;
     if( $g_link )
         return $g_link;
-    $g_link = mysql_connect( $host, $user, $password) or die('Could not connect to server. ' . $host);
-    mysql_select_db($db, $g_link) or die('Could not select database.' . $db);
+    echo "g_link.\n";
+    $g_link = mysqli_connect( $GLOBALS['host'], $GLOBALS['user'], $GLOBALS['password']) or die('Could not connect to server. ' . $GLOBALS['host']);
+    
+    if( $g_link != false )
+        echo "g_link 2 \n";
+
+    mysqli_select_db($g_link, $GLOBALS['db']) or die('Could not select database.' . $GLOBALS['db']);
+    echo "g_link 3 \n";
+
     return $g_link;
 }
 
@@ -31,10 +41,20 @@ function CleanUpDB()
 {
     global $g_link;
     if( $g_link != false )
-        mysql_close($g_link);
+        mysqli_close($g_link);
     $g_link = false;
 }
 
+/*
+$mysqli = new mysqli($host, $user, $password, $db);
+echo "1 /n";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+echo "2 /n";
+$result = mysqli_query($mysqli, $sql );
+echo "3 /n";
+while ($row = $result->fetch_assoc());
+mysqli->close();
+*/
 
 
 /*$conn       = new mysqli($host, $user, $password, $db);
@@ -42,9 +62,12 @@ function CleanUpDB()
 if ($conn->connect_error) {
     die ("Database connection error: " . $conn->connect_error);
 } else {
-    */
+*/
     // $result = $conn->query("INSERT INTO $table (note) VALUES ('random note - " . rand() . "')");
-    $result = mysql_query("SELECT * FROM $table", GetMyConnection() );
+    
+    echo "before result.\n";
+    $result = mysqli_query(GetMyConnection(),$sql );
+    echo "after result.\n";
     if ($result) {
         echo "Successfully connected to webscrapping database.\n";
         #$result = $conn->query("SELECT * FROM $table");
@@ -55,3 +78,5 @@ if ($conn->connect_error) {
     } else {
         die ("Database connection error occurred. :(");
     }
+
+?>
