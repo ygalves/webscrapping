204711a1f4ed1967414be1f5f9cd0a4ad669e009
ygalves <yoniliman.galves@uao.edu.co>
2024-03-29 23:19:55 -0500

    update test.py with inserts of database

--- mlmodel/test.py
+++ mlmodel/test.py
@@ -34,15 +34,40 @@ cur.execute("Select count(row_id) as qty from user_name where user_id = ?", (som
 for qty in cur: 
     print(f"The user name: {some_name}, is active?: {qty}")
 
-"""    
-#insert information 
+   
+#insert information (for use getout triple quotes of paragraph complete )
+"""
+name = 'Alma Maria Silva de Alegria'
+nickname = 'ASilva'
+password = 'uaouser'
+comment = 'first login'
+
 try: 
-    cur.execute("INSERT INTO employees (first_name,last_name) VALUES (?, ?)", ("Maria","DB")) 
+    cur.execute("INSERT INTO `user_name`(`user_id`, `user_desc`, `encrypt_pw`, `last_edit_comment`) VALUES (?,?,PASSWORD(?),?)", (name, nickname, password, comment)) 
 except mariadb.Error as e: 
     print(f"Error: {e}")
 
 conn.commit() 
 print(f"Last Inserted ID: {cur.lastrowid}")
+"""
+"""
+url = 'www.pornhub.com'
+cat1 = 'Adult'
+cat2 = 'Education'
+cat3 = 'Commercial'
+cat4 = 'Sports'
+cat5 = 'Entertainment'
+comment = 'Test'
+name = 'Alma Maria Silva de Alegria'
+
+try: 
+    cur.execute("INSERT INTO `result`(`url_data`, `cat1`, `cat2`, `cat3`, `cat4`, `cat5`,`lat-edit_comment`, `user_id`) VALUES (?,?,?,?,?,?,?,?)", (url, cat1, cat2, cat3, cat4, cat5, comment, name)) 
+except mariadb.Error as e: 
+    print(f"Error: {e}")
+
+conn.commit() 
+print(f"Last Inserted ID: {cur.lastrowid}")
+
 """
 
 conn.close()
