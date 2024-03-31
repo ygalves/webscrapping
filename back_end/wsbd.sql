2eb9e524fe6eb7d9f92356865f6fd39419d0ad44
ygalves <yoniliman.galves@uao.edu.co>
2024-03-30 23:24:16 -0500

    SQL enforce url results table to avoid doubles registers and convert python sql results (tuplas) into integers

--- back_end/wsbd.sql
+++ back_end/wsbd.sql
@@ -3,17 +3,16 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 31-03-2024 a las 01:16:33
+-- Tiempo de generación: 31-03-2024 a las 04:19:06
 -- Versión del servidor: 11.0.5-MariaDB-1:11.0.5+maria~ubu2204
 -- Versión de PHP: 8.2.17
 
 --
--- UAO IA SPECIALIST 
--- WEBSCRAPPING DATABASE CREATING 
---  IA TASKFORCE - 2024
+-- UAO IA SPECIALIST
+-- WEBSCRAPPING DATABASE CREATING
+-- IA TASKFORCE - 2024
 -- V: 3.0.0
--- Create List table, update db structure and set user_name trigger to create default global list
-
+--
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -21,49 +20,16 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `wsdb`
 --
-CREATE DATABASE IF NOT EXISTS `wsdb` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
-USE `wsdb`;
-
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `user_name`
---
-
-DROP TABLE IF EXISTS `user_name`;
-CREATE TABLE `user_name` (
-  `row_id` bigint(20) NOT NULL,
-  `user_id` varchar(100) NOT NULL,
-  `user_desc` varchar(500) DEFAULT NULL,
-  `encrypt_pw` varchar(254) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT 1,
-  `spare1` varchar(500) DEFAULT NULL,
-  `spare2` varchar(500) DEFAULT NULL,
-  `spare3` varchar(500) DEFAULT NULL,
-  `spare4` varchar(500) DEFAULT NULL,
-  `spare5` varchar(500) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `last_edit_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `last_edit_comment` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Disparadores `user_name`
---
-DROP TRIGGER IF EXISTS `TRG_list`;
-DELIMITER $$
-CREATE TRIGGER `TRG_list_usrn` AFTER INSERT ON `user_name` FOR EACH ROW INSERT INTO `list`(`list_name`, `user_id`, `last_edit_comment`) VALUES ('global',NEW.row_id,'root')
-$$
-DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `list`
 --
+-- Creación: 31-03-2024 a las 02:43:26
+-- Última actualización: 31-03-2024 a las 02:46:14
+--
 
-DROP TABLE IF EXISTS `list`;
 CREATE TABLE `list` (
   `row_id` bigint(20) NOT NULL,
   `list_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -78,13 +44,21 @@ CREATE TABLE `list` (
   `last_edit_comment` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
+--
+-- RELACIONES PARA LA TABLA `list`:
+--   `user_id`
+--       `user_name` -> `row_id`
+--
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `result`
 --
+-- Creación: 31-03-2024 a las 03:59:05
+-- Última actualización: 31-03-2024 a las 03:57:50
+--
 
-DROP TABLE IF EXISTS `result`;
 CREATE TABLE `result` (
   `row_id` bigint(20) NOT NULL,
   `url_data` varchar(500) NOT NULL,
@@ -105,7 +79,50 @@ CREATE TABLE `result` (
   `user_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='webscrapping results from ML api';
 
+--
+-- RELACIONES PARA LA TABLA `result`:
+--   `list_id`
+--       `list` -> `row_id`
+--   `user_id`
+--       `user_name` -> `row_id`
+--
+
 -- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_name`
+--
+-- Creación: 31-03-2024 a las 04:03:04
+-- Última actualización: 31-03-2024 a las 02:46:14
+--
+
+CREATE TABLE `user_name` (
+  `row_id` bigint(20) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `user_desc` varchar(500) DEFAULT NULL,
+  `encrypt_pw` varchar(254) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `spare1` varchar(500) DEFAULT NULL,
+  `spare2` varchar(500) DEFAULT NULL,
+  `spare3` varchar(500) DEFAULT NULL,
+  `spare4` varchar(500) DEFAULT NULL,
+  `spare5` varchar(500) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `last_edit_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `last_edit_comment` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `user_name`:
+--
+
+--
+-- Disparadores `user_name`
+--
+DELIMITER $$
+CREATE TRIGGER `TRG_list_usrn` AFTER INSERT ON `user_name` FOR EACH ROW INSERT INTO `list`(`list_name`, `user_id`, `last_edit_comment`) VALUES ('global',NEW.row_id,'root')
+$$
+DELIMITER ;
 
 --
 -- Índices para tablas volcadas
@@ -124,8 +141,8 @@ ALTER TABLE `list`
 --
 ALTER TABLE `result`
   ADD PRIMARY KEY (`row_id`) USING BTREE,
-  ADD KEY `IX_user_url_rslt` (`user_id`,`url_data`) USING BTREE,
-  ADD KEY `IX_list_url_rslt` (`list_id`,`url_data`) USING BTREE;
+  ADD UNIQUE KEY `IX_list_url_usr_rslt` (`list_id`,`url_data`,`user_id`) USING BTREE,
+  ADD KEY `IX_user_url_rslt` (`user_id`,`url_data`) USING BTREE;
 
 --
 -- Indices de la tabla `user_name`
@@ -133,6 +150,7 @@ ALTER TABLE `result`
 ALTER TABLE `user_name`
   ADD PRIMARY KEY (`row_id`),
   ADD UNIQUE KEY `IX_userid_usrn` (`user_id`),
+  ADD UNIQUE KEY `IX_userdesc_usrn` (`user_desc`),
   ADD KEY `IX_rowid_usrn` (`row_id`);
 
 --
@@ -143,19 +161,19 @@ ALTER TABLE `user_name`
 -- AUTO_INCREMENT de la tabla `list`
 --
 ALTER TABLE `list`
-  MODIFY `row_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `row_id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `result`
 --
 ALTER TABLE `result`
-  MODIFY `row_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `row_id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `user_name`
 --
 ALTER TABLE `user_name`
-  MODIFY `row_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `row_id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
--- mlmodel/test.py
+++ mlmodel/test.py
@@ -20,6 +20,7 @@ except mariadb.Error as e:
 cur = conn.cursor()
 cur1 = conn.cursor()
 cur2 = conn.cursor()
+
 """
 #retrieving information 
 some_name = '%' 
@@ -37,25 +38,26 @@ for qty in cur:
     print(f"The user name: {some_name}, password is ok?: {qty}")
 """
 #retrieving confirmation of exist user
+# V3.0.0
 """
-some_name = 'joaquin'
-some_pass = 'userx'
+some_name = 'ASilva'
+some_pass = 'uaouser'
 #some_name = 'joaquin1'
 cur.execute("Select count(row_id) as qty from user_name where user_id = ? or user_desc = ?", (some_name,some_name,))   
 
 for qty in cur: 
     # print(f"The user name: {some_name}, is active?: {qty}")
-    if qty == (0,):
+    if int(''.join(map(str, qty))) == 0:
         print(f"{some_name} user not exist, please log a new user!") 
     else:
         cur1.execute("Select count(row_id) as qty from user_name where (user_id = ? or user_desc = ?) and encrypt_pw = PASSWORD(?)", (some_name,some_name,some_pass,))  
         for qty in cur1: 
-            if qty == (0,):
+            if int(''.join(map(str, qty)))  == 0:
                 print(f"Wrong password for {some_name} user") 
             else:
                 cur2.execute("Select user_desc from user_name where (user_id = ? or user_desc = ?) and encrypt_pw = PASSWORD(?)", (some_name,some_name,some_pass,)) 
                 for user_desc in cur2:
-                    print(f"WELCOME {user_desc}")
+                    print(f"WELCOME {str(''.join(map(str, user_desc)))}")
 
 """
 # insert information (for use getout triple quotes of paragraph complete )
@@ -83,8 +85,8 @@ else:
 
 # insert information (for use getout triple quotes of paragraph complete )
 ## create new url category into results table
-### V2
-
+### V3.0.0
+"""
 url = 'www.pornhub.com'
 cat1 = 'Adult'
 cat2 = 'Education'
@@ -98,16 +100,22 @@ list = 'global'
 
 try: 
     cur.execute("Select row_id as user_id from user_name where user_id = ? and user_desc = ?", (name,nickname,)) 
+except mariadb.Error as e: 
+    print(f"Error: {e}")
+
+try:   
     for user_id in cur: 
-        cur1.execute("Select list_id from list where list_id= ?", (user_id,)) 
-        for list_id in cur: 
-    cur.execute("INSERT INTO `result`(`url_data`, `cat1`, `cat2`, `cat3`, `cat4`, `cat5`,`lat-edit_comment`, `user_id`) VALUES (?,?,?,?,?,?,?,?)", (url, cat1, cat2, cat3, cat4, cat5, comment, name)) 
+        cur1.execute("Select row_id as list_id from list where user_id = ? and list_name = ?", (int(''.join(map(str, user_id))),list))
+        print(f"user_id {user_id} was figureout for user {nickname}")
+        for list_id in cur1: 
+            cur2.execute("INSERT INTO `result`(`url_data`, `cat1`, `cat2`, `cat3`, `cat4`, `cat5`,`last_edit_comment`, `list_id`, `user_id`) VALUES (?,?,?,?,?,?,?,?,?)", (url, cat1, cat2, cat3, cat4, cat5, comment, int(''.join(map(str, list_id))), int(''.join(map(str, user_id))),)) 
+            print(f"list_id {list_id} was figureout for list {list}")
 except mariadb.Error as e: 
     print(f"Error: {e}")
 
 conn.commit() 
-print(f"Last Inserted ID: {cur.lastrowid}")
-
+print(f"Last Inserted ID: {cur2.lastrowid}")
+"""
 
 # close database connection
 conn.close()
